eae6e02d91b0850b934687033e63718be8271663
coryortega <coryortega3@gmail.com>
2019-10-04 09:01:10 -0700

    finished header and tabs

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+axios
+.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response)
+//   response.data.topics.forEach(item => {
+
+//       const tabContainer= createMenu(item);
+//       topic.appendChild(tabContainer);
+
+    // });
+  })
+.catch(error => {
+  console.log("The data was not returned", error);
+});
+
+function cards(article) {
+
+    const 
+    card = document.createElement("div"),
+    headline = document.createElement("div"),
+    author = document.createElement("div"),
+    imgContainer = document.createElement("div"),
+    img = document.createElement("img"),
+    authorsName = document.createElement("span");
+
+    date.textContent ="SMARCH 28, 2019";
+    title.textContent = "98°"
+    header.classList.add("header");
+    date.classList.add("date");
+    temperature.classList.add("temp");
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temperature);
+
+    return header;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topic = document.querySelector(".topics");
+
+  axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    response.data.topics.forEach(item => {
+  
+        const tabContainer= createMenu(item);
+        topic.appendChild(tabContainer);
+
+      });
+    })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+function createMenu(tab) {
+
+    const 
+    tabs = document.createElement("div");
+
+    tabs.textContent = tab
+    tabs.classList.add("tab");
+
+return tabs
+}
